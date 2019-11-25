818034dec7f691eba94091a47eb620c88c30350b
Tarık Ahmet Ersoylu <t-ersoylu41@hotmail.com>
2019-11-25 21:27:02 +0300

    Test Okutma Sayfası Tasarımı Yapıldı

--- anaSayfa.php
+++ anaSayfa.php
@@ -10,7 +10,8 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script type="text/javascript" language="javascript" src="Script.js"></script>
   <link href="css.css" rel="stylesheet">
-  <link href="table.css" rel="stylesheet">
+  <link href="testOkut.css" rel="stylesheet">
+  
 
   <title>Kullanıcılar</title>
 
@@ -20,113 +21,67 @@
   <!-- Custom styles for this template -->
 
 </head>
-<?php
+
+<body>
+  <div id="TumSayfa">
+	  <?php
 include "leftMenu.php";
 echo $leftMenu;
 include "readFile.php";
 $ogrenciler = readStudents();
 ?>
 
-<body>
-  <div id="TumSayfa">
     <div id="Icerik">
-      <div id="GenelTabloSinirlamaAlani">
-        <div class="col-md-12">
-
-          <h3 class="title-5 m-b-35">Kullanıcı Bilgileri</h3>
-          <div class="table-data__tool">
-
-            <div class="table-data__tool-right">
-              <button class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#myModelUserAdd"><i class="zmdi zmdi-plus"></i>Yeni Kullanıcı Ekle</button>
-            </div>
-          </div>
-          <div class="table-responsive table-responsive-data2">
-            <table id="TabloSinirlamaAlani" class="table table-data2">
-              <thead>
-                <tr>
-                  <th>Adı</th>
-                  <th>Soyadi</th>
-                  <th>Numarası</th>
-                  <th>Grubu</th>
-                  <th>Cevaplar</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                for ($i = 0; $i < count($ogrenciler); $i++) {
-
-                  echo '
-    <tr class="tr-shadow">
-      <td>' . $ogrenciler[$i][0] . '</td>
-      <input id="hangiOgrenciID" type="hidden" class="form-control" name="hangiOgrenci" value="' . $i . '">
-      <td>' . $ogrenciler[$i][1] . '</td>
-      
-      <td>' . $ogrenciler[$i][2] . '</td>
-      
-      <td>' . $ogrenciler[$i][3] . '</td>
-      
-      <td>
-        <div class="table-data-feature">
-         <button class="au-btn au-btn-icon au-btn--darkseagreen au-btn--small" data-toggle="modal" data-target="#cevapModal">
-            Göster
-          </button>
-        </div>
-      </td>
-    </tr>
-    <tr class="spacer"></tr>';
-                }
-                ?>
-                <script>
-
-                </script>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+			<div class="form-group" id="testOkutma" >
+				
+ 				<label  style="float:left;">Bölüm:</label>
+  				<select class="form-control sel1" >
+    				<option>1</option>
+    				<option>2</option>
+  				</select>
+				
+				
+				<label style="float:left;">Yarıyıl:</label>
+  				<select class="form-control sel1" >
+    				<option>1</option>
+    				<option>2</option>
+  				</select>
+				
+				
+				<label style="float:left;">Ders:</label>
+  				<select class="form-control sel1" >
+    				<option>1</option>
+    				<option>2</option>
+  				</select>
+				
+				
+				<label  style="float:left;">Sınav:</label>
+  				<select class="form-control sel1">
+    				<option>1</option>
+    				<option>2</option>
+  				</select>
+				
+				
+				<label  style="float:left;">Öğrenci Cevap Kağıdı:</label>
+  				<div class="custom-file  sel1">
+    				<input type="file" class="custom-file-input sel1" >
+    				<label class="custom-file-label" >Dosya Seç</label>
+ 				 </div>
+				
+				
+				<label  style="float:left;">Cevap Anahtarı:</label>
+  				<div class="custom-file sel1">
+    				<input type="file" class="custom-file-input" >
+    				<label class="custom-file-label" >Dosya Seç</label>
+ 				 </div>
+				<button type="button" class="btn btn-primary" style="float:right; margin-top:20px; width:80px;">Okut</button>
+				
+			</div>
     </div>
   </div>
-  </div>
-  </div>
-  <!-- CEVAPLAR MODAL-->
-  <div class="modal fade" id="cevapModal">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-md">
-      <div class="modal-content">
+  
 
-        <!-- Modal Header -->
-        <div class="modal-header">
-          <h4 class="modal-title">Cevapları</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
 
-        <!-- Modal body -->
-        <div class="modal-body">
-          <div class="table-responsive table-responsive-data2">
-            <table id="TabloSinirlamaAlani" class="table table-data2">
-              <thead>
-                <tr>
-                  <?php
-                  $cevapAnahtari = readCevapAnahtari();
-                  $cevaplar = explodeEachChar($cevapAnahtari[0][1]);
-                  for ($i = 1; $i < count($cevaplar) + 1; $i++) {
-                    echo '<th>' . $i . '</th>';
-                  }
-                  ?>
-                </tr>
-                <tr>
-                  <?php
-                  $ogrencininCevaplari = whichStudent(0);
-                  $ogrenciCevaplar = explodeEachChar($ogrencininCevaplari[4]);
-                  for ($i = 0; $i < count($ogrenciCevaplar); $i++) {
-                    echo '<th>' . $ogrenciCevaplar[$i] . '</th>';
-                  }
-                  ?>
-                </tr>
-              </thead>
-          </div>
-        </div>
-      </div>
-    </div>
 </body>
 
 </html>
